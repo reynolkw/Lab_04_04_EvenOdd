cc57dd874a5594674c91e6c9367a33dc547367f2
hueforge <111766391+reynolkw@users.noreply.github.com>
2023-09-16 23:14:47 -0400

    added functionality

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // simulate user input
+        int numberToCheck = 569247;
+
+        System.out.println("Results (0 = even, 1 = odd): " + numberToCheck % 2);
+    }
+}
